f99f8a1e9d547704e7a84104fb82f6f45d81b7a2
Nathan Marsh <nathan.marsh@intapp.com>
2020-02-21 14:01:15 -0500

    safety check for directory path

--- src/buildReferenceFilesAsync.js
+++ src/buildReferenceFilesAsync.js
@@ -1,18 +1,27 @@
-const fs = require('fs');
-const getDCPropName = require('./getDCPropName');
-const classBuilder = require('./classBuilder');
+const fs = require("fs");
+const getDCPropName = require("./getDCPropName");
+const classBuilder = require("./classBuilder");
 module.exports = async (dcConnector, dcClient, schemaPath, classPath) => {
   let schema = await dcConnector.getSchema(dcClient);
-  
+
   if (!fs.existsSync(schemaPath)) {
     fs.mkdirSync(schemaPath);
   }
+
+  if (schemaPath[schemaPath.length - 1] !== "/") {
+    schemaPath += '/'
+  }
+
+  if (classPath[classPath.length - 1] !== "/") {
+    classPath += '/'
+  }
+
   schema.forEach(sch => {
     let fileName = sch.list.replace(/[^a-zA-Z0-9]/g, "");
-    sch.fields.forEach((field) => {
+    sch.fields.forEach(field => {
       field.jsonProperty = getDCPropName(field.Name, field.FieldType);
     });
     fs.writeFileSync(schemaPath + fileName + ".json", JSON.stringify(sch));
-    fs.writeFileSync(classPath + fileName + '.js', classBuilder(sch));
+    fs.writeFileSync(classPath + fileName + ".js", classBuilder(sch));
   });
-}
+};
