7015d1463a78c1f67b9286d6d5a9e0c5bacb707f
Nathan Marsh <nathan.marsh@intapp.com>
2020-03-10 11:13:09 -0400

    fix for async issue

--- src/processDCPullAsync.js
+++ src/processDCPullAsync.js
@@ -4,12 +4,11 @@ module.exports = async (dcConnector, dcClient, dcPulls, batchSize = 10000) => {
   let batchedRequests = getBatchedRequests(dcPulls, batchSize);
   let results = [];
   await myUtils.asyncForEach(batchedRequests, async request => {
-    results = results.concat(
-      await dcConnector.processDCPullAsync({
-        client: dcClient,
-        DCPulls: request
-      })
-    );
+    let temp =  await dcConnector.processDCPullAsync({
+      client: dcClient,
+      DCPulls: request
+    });
+    results = results.concat(temp);
   });
 
   return results;
--- src/processDCPushAsync.js
+++ src/processDCPushAsync.js
@@ -1,16 +1,21 @@
 const getDCPushRequests = require("./getDCPushRequests");
-module.exports = async function(dcConnector, dcClient, dcObjects, entryListId, batchSize = 10000) {
+module.exports = async function(
+  dcConnector,
+  dcClient,
+  dcObjects,
+  entryListId,
+  batchSize = 10000
+) {
   let dcPushes = getDCPushRequests(dcObjects);
   let batchedRequests = getBatchedRequests(dcPushes, batchSize);
   let results = [];
   await myUtils.asyncForEach(batchedRequests, async request => {
-    results = results.concat(
-      await dcConnector.processDCPushAsync({
-        client: dcClient,
-        entryListId: entryListId,
-        DCPulls: request
-      })
-    );
+    let temp = await dcConnector.processDCPushAsync({
+      client: dcClient,
+      entryListId: entryListId,
+      DCPulls: request
+    });
+    results = results.concat(temp);
   });
 
   return results;
